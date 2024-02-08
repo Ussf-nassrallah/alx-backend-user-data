0e52abf28aa7600431cf8f11b63dc2df3e6ab0fc
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-08 10:39:47 +0100

    create: get_db() -> method thats connect with mysql database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,16 +66,16 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """get_db method thats connect with mysql database"""
 
-    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME") or "root"
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD") or ""
+    host = os.getenv("PERSONAL_DATA_DB_HOST") or "localhost"
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
-    database = mysql.connector.connect(
+    connect = mysql.connector.connect(
         user=username,
         password=password,
         host=host,
         database=db_name
     )
 
-    return database
+    return connect
