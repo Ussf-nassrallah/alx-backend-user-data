65e170b3ce0f07a2da0b88589d24c225cebf3ba1
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-08 11:03:17 +0100

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,12 +6,11 @@ from bcrypt import hashpw
 
 def hash_password(password: str) -> bytes:
     ''' function thats hashed password '''
-    encode_pssw = password.encode()
-    hashed_pass = hashpw(b, bcrypt.gensalt())
+    hashed_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed_pass
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     ''' is_valid: function that checks if password is valid or not '''
-    checked_pssw = bcrypt.checkpw(password.encode(), hashed_password)
+    checked_pssw = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
     return checked_pssw
