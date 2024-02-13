e2a2a9c403015f2a022093e4dc2e571c88abf9a5
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-13 15:27:43 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 ''' basic_auth module '''
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,20 @@ class BasicAuth(Auth):
                 or authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        ''' decode_base64_authorization_header method '''
+        if base64_authorization_header is None\
+                or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_hdr_auth = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+            )
+            return decoded_hdr_auth.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
