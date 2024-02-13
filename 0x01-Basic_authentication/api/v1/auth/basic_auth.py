789f92abae75bae22409f1da4b848292eadc1d03
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-13 15:37:45 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+import re
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -34,3 +36,23 @@ class BasicAuth(Auth):
             return decoded_hdr_auth.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str,
+    ) -> Tuple[str, str]:
+        ''' decoded value of a Base64 string '''
+        if isinstance(decoded_base64_authorization_header, str):
+            user_info = r'(?P<user>[^:]+):(?P<password>.+)'
+
+            fields = re.fullmatch(
+                user_info,
+                decoded_base64_authorization_header.strip(),
+            )
+
+            if fields is not None:
+                user = fields.group('user')
+                password = fields.group('password')
+                return (user, password)
+
+        return None, None
