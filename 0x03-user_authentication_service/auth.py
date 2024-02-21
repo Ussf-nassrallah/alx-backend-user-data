77c55398d7b19f77810eefb27bffc9a026e475a2
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-21 10:03:57 +0100

    add: _hash_password method in auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' auth module '''
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    ''' hash user password '''
+    user_pssw = password.encode('utf-8')
+    encoded_user_pssw = bcrypt.hashpw(user_pssw, bcrypt.gensalt())
+    return encoded_user_pssw
