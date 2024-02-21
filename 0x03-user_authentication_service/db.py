35133ce32673c047b5ecc6d1a26d1527949492e3
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-21 09:56:28 +0100

    add: update_user method in DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,7 +50,7 @@ class DB:
                     return user
         raise NoResultFound
 
-    def update_user(self, user_id: int, **kwargs) -> User:
+    def update_user(self, user_id: int, **kwargs) -> None:
         ''' update user info '''
         try:
             user = self.find_user_by(id=user_id)
