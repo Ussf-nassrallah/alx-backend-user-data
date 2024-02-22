1e3e16eaab5a09d43896a2198da04584ee7b656b
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-22 10:15:27 +0100

    handle: user login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' set up a Flask app '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,22 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{user_email}", "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    ''' handle user login '''
+    user_email = request.form.get('email')
+    user_passw = request.form.get('password')
+
+    user = auth.valid_login(user_email, user_passw)
+
+    if not user:
+        abort(401)
+
+    auth.create_session(user_email)
+
+    return jsonify({"email": user_email, "message": "logged in"})
 
 
 if __name__ == '__main__':
