67c7bdd4a9908920fca6512ce441fc758820e16f
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-22 12:24:33 +0100

    update user password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,3 +94,13 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        ''' update user password '''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hash_user_pssw = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hash_user_pssw)
+        reset_token = None
